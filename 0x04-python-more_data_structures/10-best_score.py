2b53a7b1681248b36c663aad3985ef7a385cd016
Mario Nageh <marionageh7@gmail.com>
2023-06-14 14:58:01 -0700

    10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def best_score(a_dic):
-    return max(a_dict, key=a_dict.get) if a_dic else None
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None

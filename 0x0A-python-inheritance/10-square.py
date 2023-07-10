85d920f6dbd5c05b63732264f477083009074ff2
Mario Nageh <marionageh7@gmail.com>
2023-07-11 01:20:19 +0300

    Square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This Is Square Module"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """This Is Square Class"""
+
+    def __init__(self, size):
+        """This Is Square Constructor"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)

abfbe068648c2785a9f710e9fdfded2a83e34d2f
Mario Nageh <marionageh7@gmail.com>
2023-07-10 19:02:53 +0300

    MyList Class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/bin/usr/python3
+"""This Module is New Class Instance of list
+with some functionality
+"""
+
+
+class MyList(list):
+    """This is New Class Instance of list
+    with some functionality
+    """
+    def print_sorted(self):
+        """Prints the list in sorted order Ascending sorted order"""
+        print(sorted(self))
+
+    def append(self, __object):
+        if not isinstance(__object, int):
+            raise TypeError("You Must Insert Integer")
+        return super().append(__object)

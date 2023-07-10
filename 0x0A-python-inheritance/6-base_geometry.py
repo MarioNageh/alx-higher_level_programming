b729321aea1d48e09d6a4021c350705e92be6a20
Mario Nageh <marionageh7@gmail.com>
2023-07-10 19:58:44 +0300

    Abstract Method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Empty Class"""
+
+
+class BaseGeometry:
+    """This Class is Base Geometry class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")

85e75639de2936da3f81322275552f34dfd826f5
Mario Nageh <marionageh7@gmail.com>
2023-07-10 19:41:46 +0300

    is kind  of

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This File Checks if the specified instance of Class"""
+
+
+def is_kind_of(obj, a_class):
+    """Checks if the specified instance of Class"""
+    return isinstance(obj, a_class)

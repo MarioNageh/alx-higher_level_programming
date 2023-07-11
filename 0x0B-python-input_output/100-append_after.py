193ba20c9cdca027d1285169a67b547206aa4dd1
Mario Nageh <marionageh7@gmail.com>
2023-07-12 01:11:44 +0300

    Append Af ter

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""append after module"""
+
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Insert a new string of after search string
+    """
+    with open(filename, 'r+') as file:
+        lines = file.readlines()
+        file.seek(0)
+
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')

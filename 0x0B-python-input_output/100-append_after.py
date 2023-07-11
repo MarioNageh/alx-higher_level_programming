98c1aefa9e057f8cbed63cd93a97591e81d8db53
Mario Nageh <marionageh7@gmail.com>
2023-07-12 01:19:36 +0300

    Fixing

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,8 +7,10 @@ def append_after(filename="", search_string="", new_string=""):
     '''
     with open(filename, 'r+') as f:
         lines = f.readlines()
+        i = 0
         for line in lines:
-            f.write(line)
-            if search_string in line:
-                f.write(new_string)
-                f.write('\n')
+            if line.find(search_string) is not -1:
+                lines.insert(i + 1, new_string)
+            i += 1
+        f.seek(0)
+        f.write("".join(lines))

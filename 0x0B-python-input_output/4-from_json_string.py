00115599b6486cb7367726a4c413f75a958767f7
Mario Nageh <marionageh7@gmail.com>
2023-07-11 17:02:13 +0300

    Convert Json String To Python

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""this function to convert a string to json"""
+import json
+
+
+def from_json_string(my_obj):
+    """this function to convert a string to json"""
+    return json.loads(my_obj)

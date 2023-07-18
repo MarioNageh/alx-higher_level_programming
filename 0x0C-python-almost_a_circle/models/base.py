b44143e87d15802567fb3c8e0886ac2976d9ade1
Mario Nageh <marionageh7@gmail.com>
2023-07-18 03:21:39 +0300

    to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This Module Is Python Base Class"""
+import json
 
 
 class Base:
@@ -13,3 +14,10 @@ class Base:
         else:
             Base._nb_objects += 1
             self.id = Base._nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        ''' convert list of dict represent objects to json string '''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)

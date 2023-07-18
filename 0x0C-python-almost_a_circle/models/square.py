a5fb53d8683656492a8b901a496b429a9ea68cb1
Mario Nageh <marionageh7@gmail.com>
2023-07-18 03:13:07 +0300

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """this is square Classification"""
 from .rectangle import Rectangle
 
+
 class Square(Rectangle):
     """this is square class"""
 
@@ -13,3 +14,14 @@ class Square(Rectangle):
         """this is Square String Representation"""
         return f"[Square] ({self.id}) {self.x}/{self.y}"\
             f" - {self.width}"
+
+    @property
+    def size(self):
+        """this is Square size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """this is Square size setter"""
+        self.width = value
+        self.height = value

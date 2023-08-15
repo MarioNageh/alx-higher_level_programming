3a090ff93a9a30e21253e78abf87770a7efe94b4
Mario Nageh <marionageh7@gmail.com>
2023-08-15 22:52:16 +0300

    Fixing

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
 -- List All Record That Score >= 10
-SELECT score , name from `second_table` where score >= 10 order by score desc;
+SELECT `score` , `name` FROM `second_table`
+WHERE `score` >= 10 ORDER BY `score` DESC;
--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- update Pop Score
-update `second_table` set `score` = 10 where `name` = "Bob";
+UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";

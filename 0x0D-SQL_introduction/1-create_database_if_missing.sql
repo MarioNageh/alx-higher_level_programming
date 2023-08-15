2da8c151e856c48be5176ba930436fa29fc0d5a4
Mario Nageh <marionageh7@gmail.com>
2023-08-15 22:10:26 +0300

    First 4 Tasks

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- create Database if exists not fail
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- CREATE First Table `first_table` in `hbtn_0c_0` Database
+CREATE TABLE IF NOT EXISTS `first_table` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
+);

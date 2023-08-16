857608a529fe80ed7f524cd4f6ca920487240f7e
Mario Nageh <marionageh7@gmail.com>
2023-08-17 00:57:53 +0300

    Tasks 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- My Privileges
-show grants for 'user_0d_1'@'localhost';
-show grants for 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

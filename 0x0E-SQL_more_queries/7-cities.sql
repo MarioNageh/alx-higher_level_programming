c9cb39836d17d6a499b38e79017c12fbb277c15d
Mario Nageh <marionageh7@gmail.com>
2023-08-17 11:49:56 +0300

    TASK 6,7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Create DATABASE , TABLES
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`
+(
+    `id` INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(256) NOT NULL,
+    `state_id` INT NOT NULL,
+    FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`)
+);

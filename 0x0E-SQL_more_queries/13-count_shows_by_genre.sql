05881075cff52a4df235c0ac063fb50d4a2b2953
Mario Nageh <marionageh7@gmail.com>
2023-08-17 15:17:46 +0300

    Count & Group

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- Count The Shows By Genre
+SELECT tv_genres.name AS genre , count(tv_show_genres.show_id) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;

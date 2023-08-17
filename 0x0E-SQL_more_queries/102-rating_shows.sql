75771fe277ab196442e90535718fb4168ee06c35
Mario Nageh <marionageh7@gmail.com>
2023-08-17 16:01:39 +0300

    Advanced

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- Show Ratting
+SELECT tv_shows.title AS title, sum(tv_show_ratings.rate) as rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id=tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;

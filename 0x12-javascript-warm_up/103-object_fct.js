7353c663d248b99027ffbaa50d18a862a6b0ac48
Mario Nageh <marionageh7@gmail.com>
2023-09-12 01:24:40 +0300

    add function to object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+myObject.incr = () => this.value++;
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);

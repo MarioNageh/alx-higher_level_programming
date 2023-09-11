bd5e1daab8a71e2ab53561f5a0becdcabb72cc01
Mario Nageh <marionageh7@gmail.com>
2023-09-12 00:27:40 +0300

    Fixing

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-// Print 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
-const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const langs = ["C is fun", "Python is cool", "Javascript is amazing"];
 
 for (let i = 0; i < 3; i++) {
-  console.log(lines[i]);
+  console.log(langs[i]);
 }

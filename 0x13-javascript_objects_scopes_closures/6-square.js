29ffe00b448e1dbcca161a5e2698bc0dc85d0134
Mario Nageh <marionageh7@gmail.com>
2023-09-13 05:52:00 +0300

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const S = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends S {
   charPrint (c) {
     if (c === undefined) c = 'X';
     console.log((c.repeat(this.width) + '\n').repeat(this.height).slice(0, -1));

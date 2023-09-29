333c5294383377bc77f9450b0f1abcb1bfa39581
Mario Nageh <marionageh7@gmail.com>
2023-09-29 05:11:10 +0300

    Find Peak Fxing ;

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,7 @@ def find_peak(list_of_integers):
     Find Biggest Number That Before and After Below This
     or Equals
     """
+    
     current_max_peak = None
     for i in range (0,len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i+1]:

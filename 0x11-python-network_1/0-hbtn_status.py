3825497cc052ba9e7a6d281be7eb356add988583
Mario Nageh <marionageh7@gmail.com>
2023-09-30 05:07:43 +0300

    Response Status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+url = 'https://intranet.hbtn.io/status'
+with urllib.request.urlopen(url) as response:
+    encoding = response.headers.get_content_charset()
+    body = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
+    print('\t- utf8 content: {}'.format(body.decode(encoding)))

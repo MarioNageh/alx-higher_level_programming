afa2e293bd87984505f5403eb3b71e107d8b0c35
Mario Nageh <marionageh7@gmail.com>
2023-09-30 05:56:42 +0300

    Advanced

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    url = f'https://api.github.com/repos/{user}/{repo}/commits'
+    headers = {"Accept": "application/vnd.github.v3+json",
+               "X-GitHub-Api-Version": "2022-11-28",
+               }
+    response = requests.get(url, headers=headers)
+    body = response.json()
+    for commit in body:
+        print(commit.get("sha"), end=": ")
+        print(commit.get("commit").get("author").get("name"))

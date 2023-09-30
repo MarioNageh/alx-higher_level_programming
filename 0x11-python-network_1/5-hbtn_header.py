844e4d1b1e2e3468beb1ffa1c41797e85bf26d21
Mario Nageh <marionageh7@gmail.com>
2023-09-30 05:29:38 +0300

    Task 7

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,5 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    data = {"email": sys.argv[2]}
-    req = requests.post(url, data=data)
-    print(req.text)
+    req = requests.post(url)
+    print(req.headers["X-Request-Id"])
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -5,5 +5,5 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    req = requests.get(url)
-    print(req.headers.get('X-Request-Id'))
+    req = requests.get(url, data={'email': sys.argv[2]})
+    print(req.text)

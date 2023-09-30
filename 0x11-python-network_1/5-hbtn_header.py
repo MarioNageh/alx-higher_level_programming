d29876bc62cbd36fec60f5b5284f6e32cb645332
Mario Nageh <marionageh7@gmail.com>
2023-09-30 05:25:04 +0300

    Task 4,5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers.get('X-Request-Id'))

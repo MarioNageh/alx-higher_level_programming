3e6284bbe0b6beb17b933bb898b433092c4cdaea
Mario Nageh <marionageh7@gmail.com>
2023-10-25 01:06:13 +0300

    Get Request And Save Content To File

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 // get status of url
 const request = require('request');
 const url = process.argv[2];
+if (!url) process.exit();
 request.get(url, (error, response, body) => {
   if (!error) {
     const count = body.split('/people/18/').length - 1;
